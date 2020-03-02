310faed8cbdad07836ae57c1a580b9a86dbfa890
Cirhuza Alain <cirhuzalain@gmail.com>
2020-03-02 09:28:50 +0000

    Fix typos

--- src/App.js
+++ src/App.js
@@ -252,7 +252,7 @@ class App extends Component {
                       <p className="text-muted">
                       Alain is an engineer with strong passion and interest for machine
                       learning and its broader applications to have societal impact. 
-                      He is currently undergoing the African Masters in Machine Intelligence (<a href="https://aimsammi.org/" rel="noreferrer noopener" target="_blank">AMMI</a>).  
+                      He is currently undergoing the African Master's in Machine Intelligence (<a href="https://aimsammi.org/" rel="noreferrer noopener" target="_blank">AMMI</a>).  
                       He has extensively worked on several projects
                       including but not limited to designing, implementing and
                       deploying web / mobile applications. Throughout his career, he
