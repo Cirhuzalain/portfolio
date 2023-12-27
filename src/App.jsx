c505bece781760b8b23eafd17f5f2ccacd70151a
Alain Muhigiri Cirhuza <cirhuzalain@gmail.com>
2023-12-27 10:44:24 -0800

    Add blog blueprint

--- src/App.jsx
+++ src/App.jsx
@@ -263,7 +263,7 @@ export default function App() {
                     <CardBody profile>
                       <p className="text-muted">
                           Alain is an Entrepreneur, a Researcher & a Software Engineer with strong passion and interest for Machine Learning more broadly
-                          and Natural Language Understanding more specifically, its broader applications (Entertainment, Health, Mobility, ...) to have societal impact
+                          and Natural Language Processing / Understanding more specifically, its broader applications (Entertainment, Health, Mobility, ...) to have societal impact
                           in the realm of questions pertaining to awareness, adaptation, robustness, fairness, explainability, security, privacy, monitoring, ... (M^3A2RFESPLP, ...).
                           He previously spent some time at <a href="https://aimsammi.org/" rel="noreferrer noopener" target="_blank">AIMS</a> thinking about the foundation of Machine Intelligence.
                           He has extensively worked on several projects
---
+++ src/components/BlogDetails.jsx
@@ -0,0 +1,18 @@
+import React, {userRef} from "react";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+
+export default function BlogDetails(){
+
+    const navData = userRef(null);
+
+    return (
+        <>
+            <Header navInfo={navData} />
+                <div>
+                    <p>Blog Details ...</p>
+                </div>
+            <Footer />
+        </>
+    )
+}
