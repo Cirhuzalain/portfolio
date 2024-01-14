d6329e9ce23f83584777f715509c0c5f5689b9b0
Alain Muhigiri Cirhuza <cirhuzalain@gmail.com>
2024-01-14 06:14:41 -0800

    Update blog detail

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -2,14 +2,12 @@ import React, {useRef} from "react";
 import Header from "./Header.jsx";
 import LinkMeta from "./LinkMeta.jsx";
 import Footer from "./Footer.jsx";
+import pageDetailData from "../utils.jsx";
 
 export default function Blogs(){
 
     const navData = useRef(null);
-    const pageDetailData = [{id : 1, name : "CDS", description : "", content : ""},
-                            {id : 2, name : "LP", description : "", content : ""},
-                            {id : 3, name : "RPA", description : "", content : ""},
-                            {id : 4, name : "UQ", description : "", content : ""}];
+
     return (
         <>
             <html lang="en">
